1badafca7b405bf020b2e7d90099c6933a4446d6
Valdeci Gomes <coderade@outlook.com>
2017-11-27 22:47:58 -0200

    Add the ServiceRegistry class to keep track of all services.

--- server/service.js
+++ server/service.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const service = express();
+const ServiceRegistry = require('./serviceRegistry');
+
+const serviceRegistry = new ServiceRegistry();
 
 service.put('/service/:intent/:port', (req, res, next) => {
     const serviceIntent = req.params.intent;
     const servicePort = req.params.port;
-
 
     //check if it's in IPv6 notation with a double colon and put the IP address in curly brackets. In any other case, simply return the
     //remote address
     const serviceIp = req.connection.remoteAddress.includes('::')
         ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
 
-    res.json({result: `${serviceIntent} at ${serviceIp}:${servicePort}`})
+
+    serviceRegistry.add(serviceIntent, serviceIp, servicePort);
+
+    res.json({result: `${serviceIntent} at ${serviceIp}:${servicePort}`});
 });
 
 module.exports = service;
