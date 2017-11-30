eb0fbf9e40628d10ef275e14ae1d7994cdfd73d2
Valdeci Gomes <coderade@outlook.com>
2017-11-29 23:46:50 -0200

    Add Weather intent to the project

---
+++ server/intents/weatherIntent.js
@@ -0,0 +1,24 @@
+const request = require('superagent');
+
+module.exports.process = (intentData, registry, cb) => {
+    if (intentData.intent[0].value !== 'weather')
+        return cb(new Error(`Expected weather intent, got ${intentData.intent[0].value}`));
+    if (!intentData.location)
+        return cb(new Error('Missing location intent'));
+
+    const location = intentData.location[0].value;
+    const service = registry.get('weather');
+
+    if (!service)
+        return cb(false, 'No service available');
+
+    request(`http://${service.ip}:${service.port}/service/${location}`, (err, res) => {
+        if (err || res.statusCode !== 200 || !res.body.result) {
+            console.log(err);
+
+            return cb(false, `I had a problem finding out the weather in ${location}`);
+        }
+
+        return cb(false, `The current weather in ${location} is ${res.body.result}`);
+    });
+};
